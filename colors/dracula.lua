b6aab0276dc0a596741567a21c4a0594c8941b4c
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-12-26 13:03:34 +0100

    add dracula theme

---
+++ colors/dracula.lua
@@ -0,0 +1,11 @@
+local farbe = require('farbe')
+farbe._color('dracula', {
+  bg = { '#282A36', '#191A21' },
+  fg = { '#F8F8F2', '#F8F8F2' },
+  red = { '#FF5555', '#ff6160' },
+  green = { '#50FA7B', '#5aff85' },
+  yellow = { '#F1FA8C', '#ffff82' },
+  blue = { '#8a87ff', '#b4b0ff' },
+  purple = { '#BD93F9', '#FF79C6' },
+  cyan = { '#8BE9FD', '#abfffd' },
+})
